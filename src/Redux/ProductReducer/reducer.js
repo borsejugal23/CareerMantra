181a77bcee5f437647866d5490c715bbba6cc577
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2023-05-05 12:55:36 +0530

    yesteday code added

--- src/Redux/ProductReducer/reducer.js
+++ src/Redux/ProductReducer/reducer.js
@@ -1,8 +1,21 @@
+import { GET_FAILURE, GET_REQUEST, GET_SUCCESS } from "./actionTypes"
+
 const initialstate={
-    
+    isLoading:false,
+    isError:false,
+    products:[]
 }
 export const reducer=(state=initialstate,{type,payload})=>{
     switch(type){
+      case GET_REQUEST:{
+        return{...state,isLoading:true}
+      }
+      case GET_SUCCESS:{
+        return {...state,isLoading:false,products:payload}
+      }
+      case GET_FAILURE:{
+        return {...state,isLoading:false,isError:true}
+      }
       default: return state
     }
 }
