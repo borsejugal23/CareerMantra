2e78a629468691722ef130c784f9132aeb220cdc
ShaktiSingh2605 <dxxshakirathore@gmail.com>
2023-05-06 23:44:48 +0530

    nav

--- src/components/MainRoutes.jsx
+++ src/components/MainRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import { ProductList } from "./ProductList"
 import CartPage from "../pages/CartPage"
 import SinglePage from "../pages/SinglePage"
+import HomePage from "../pages/HomePage"
 
 export const MainRoutes=()=>{
  return   <Routes>
-    <Route path="/" element={<ProductList/>}/>
+   <Route path="/" element={<HomePage />} />
+    <Route path="/product" element={<ProductList/>}/>
     <Route path="/cart" element={<CartPage/>}/>
-    <Route path="/single" element={<SinglePage/>}/>
+    <Route path="/single/:id" element={<SinglePage/>}/>
     {/* <Route path="/" element={}/>
     <Route path="/" element={}/>
     <Route path="/" element={}/>
