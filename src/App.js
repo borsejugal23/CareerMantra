8f51a8f2ab1a1232df91b056d8c071b4c0744bbf
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2023-05-09 23:04:57 +0530

    get data from main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 import  Navbar  from './components/Navbar'
 import Footer from './components/Footer';
 import { MainRoutes } from "./components/MainRoutes";
-import { AdminMainRoutes } from './pages/AdminPages/AdminMainRoutes';
 
 
 
